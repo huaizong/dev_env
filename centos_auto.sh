368228c1329186bcc70306e23bf271521fc85f17
Wang Huaizong <huaizongfujian@gmail.com>
2013-08-19 18:34:48 +0800

    [update] add mysql-server

--- centos_auto.sh
+++ centos_auto.sh
@@ -2,7 +2,7 @@
 sh autobuild.sh
 echo "PATH=$PATH:/home/huaizong/dev/bin:" >> /etc/bashrc
 echo "export PATH" >> /etc/bashrc
-yum install -y kernel-devel SDL-devel  gcc g++ gdb valgrind git telnet ftp subversion  ibus-pinyin-open-phrase.x86_64 ibus-pinyin.x86_64 libmemcached-devel libevent-devel mysql-devel
+yum install -y kernel-devel SDL-devel  gcc g++ gdb valgrind git telnet ftp subversion  ibus-pinyin-open-phrase.x86_64 ibus-pinyin.x86_64 libmemcached-devel libevent-devel mysql-devel sqlite3-devel curl-devel mysql-server
 ssh-keygen -t rsa -C "huaizongfujian@gmail.com"
 git config --global user.email huaizongfujian@gmail.com
 git config --global user.name "Wang Huaizong"
